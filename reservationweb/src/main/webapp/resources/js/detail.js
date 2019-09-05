01b8e1addb94584bd0d2a20e5fa7ce312b2977a3
hscom96 <hscom96@gmail.com>
2019-09-06 01:26:45 +0900

    수정 : 전체코드 객체리터럴 패턴으로 수정

--- reservationweb/src/main/webapp/resources/js/detail.js
+++ reservationweb/src/main/webapp/resources/js/detail.js
@@ -1,248 +1,273 @@
 document.addEventListener("DOMContentLoaded", function() {
-	updateDisplayInfoSet();
+	pageUpdateUnit.updateDisplayInfoSet();
 });
 
-function updateDisplayInfoSet() {
-	let httpRequest = new XMLHttpRequest();
-	let displayInfoSet;
-	let displayId = getParam("id");
+var pageUpdateUnit = {
+		updateDisplayInfoSet : 
+			function() {
+			let httpRequest = new XMLHttpRequest();
+			let displayInfoSet;
+			let displayId = getParam("id");
 
-	httpRequest.onreadystatechange = function() {
+			httpRequest.onreadystatechange = function() {
 
-		if (httpRequest.readyState == XMLHttpRequest.DONE
-				&& httpRequest.status == 200) {
-			displayInfoSet = JSON.parse(httpRequest.responseText);
-			
-			updateSliderImage(displayInfoSet);
-			updateProductContent(displayInfoSet);
-			updateEventInfo(displayInfoSet);
-			updateReviewHeader(displayInfoSet);
-			updateReview(displayInfoSet);
-			updateMoreReviewBtn(displayInfoSet);
-			updateDetailTab(displayInfoSet);
-			runSlider();
-			changeProductContent();
-			changeDetailTab();
+				if (httpRequest.readyState == XMLHttpRequest.DONE
+						&& httpRequest.status == 200) {
+					displayInfoSet = JSON.parse(httpRequest.responseText);
+
+					sliderUnit.updateSliderImage(displayInfoSet);
+					productContentUnit.updateProductContent(displayInfoSet);
+					eventUnit.updateEventInfo(displayInfoSet);
+					reviewUnit.updateReviewHeader(displayInfoSet);
+					reviewUnit.updateReview(displayInfoSet);
+					reviewUnit.updateMoreReviewBtn(displayInfoSet);
+					detailTabUnit.updateDetailTab(displayInfoSet);
+					sliderUnit.runSlider();
+					productContentUnit.changeProductContent();
+					detailTabUnit.changeDetailTab();
+				}
+			};
+
+			httpRequest.open("GET", "/reservationweb/api/products/" + displayId, true);
+			httpRequest.send();
 		}
-	};
-
-	httpRequest.open("GET", "/reservationweb/api/products/" + displayId, true);
-	httpRequest.send();
 }
 
-// 슬라이더 이미지 등록
-function updateSliderImage(displayInfoSet) {
-	let slider = document.querySelector(".group_visual .detail_swipe");
-	let imageTemplate = document.querySelector("#slideItemTemplate").innerHTML;
-	let bindTemplate = Handlebars.compile(imageTemplate);
-	
-	let data = {
-			"productDescription":displayInfoSet.displayInfo.productDescription,
-			"productImages":displayInfoSet.productImages.slice(0,2)
-	};
-	
-	slider.innerHTML =  bindTemplate(data);
-}
+var sliderUnit = {
+		updateSliderImage : // 슬라이더 이미지 등록
+			function(displayInfoSet) {
+			let slider = document.querySelector(".group_visual .detail_swipe");
+			let imageTemplate = document.querySelector("#slideItemTemplate").innerHTML;
+			let bindTemplate = Handlebars.compile(imageTemplate);
 
-// 슬라이더 동작
-function runSlider() {
-	let sliderBox = document.querySelector("#sliderWrap");
-	let slider = document.querySelector(".group_visual .detail_swipe");
-	let images = document.querySelectorAll(".group_visual .visual_img .item");
-	let totalCount = images.length;
-	let sliderWidth = 414;
-	let sliderIndex = 0;
+			let data = {
+					"productDescription":displayInfoSet.displayInfo.productDescription,
+					"productImages":displayInfoSet.productImages.slice(0,2)
+			};
 
-	let paginition_current = document
+			slider.innerHTML =  bindTemplate(data);
+		},
+
+		runSlider :
+			function() {
+			let sliderBox = document.querySelector("#sliderWrap");
+			let slider = document.querySelector(".group_visual .detail_swipe");
+			let images = document.querySelectorAll(".group_visual .visual_img .item");
+			let totalCount = images.length;
+			let sliderWidth = 414;
+			let sliderIndex = 0;
+
+			let paginition_current = document
 			.querySelector(".figure_pagination #current");
-	let paginition_total = document
+			let paginition_total = document
 			.querySelector(".figure_pagination .off #total");
 
-	let nextBtn = document.querySelector("div.nxt");
-	let prevBtn = document.querySelector("div.prev");
+			let nextBtn = document.querySelector("div.nxt");
+			let prevBtn = document.querySelector("div.prev");
 
-	pagination();
-	removeSlideBtn();
+			pagination();
+			removeSlideBtn();
 
-	nextBtn.addEventListener('click', function() {
-		plusSlides(1);
-		pagination();
-	});
-	prevBtn.addEventListener('click', function() {
-		plusSlides(-1);
-		pagination();
-	});
-
-	function plusSlides(n) {
-		showSlides(sliderIndex += n);
-	}
-
-	function showSlides(n) {
-		if (sliderIndex == -1) {
-			sliderIndex = totalCount - 1;
-		} else if (sliderIndex === totalCount) {
-			sliderIndex = 0;
-		}
-		
-		slider.style.transform = "translate(-" + (sliderWidth * sliderIndex) + "px,0)";
-	}
-
-	function pagination() {
-		paginition_current.innerHTML = sliderIndex + 1;
-		paginition_total.innerHTML = totalCount;
-	}
-
-	function removeSlideBtn() {
-		if (totalCount <= 1) {
-			nextBtn.parentNode.removeChild(nextBtn);
-			prevBtn.parentNode.removeChild(prevBtn);
-		}
-	}
-}
-
-//이벤트정보 업데이트
-function updateEventInfo(displayInfoSet){
-	let discountEvent = document.querySelector(".section_event .event_info_box .event_info .in_dsc");
-	let insertHTML = "[네이버예약 특별할인] <br>";
-	let temp=[];
-	
-	displayInfoSet.productPrices.forEach(value=>{
-		temp.push( value.priceTypeName+"석 "+value.discountRate+"%");
+			nextBtn.addEventListener('click', function() {
+				plusSlides(1);
+				pagination();
 			});
-	
-	insertHTML += temp.join(', ') + " 할인";
-	discountEvent.innerHTML = insertHTML;
-}
+			prevBtn.addEventListener('click', function() {
+				plusSlides(-1);
+				pagination();
+			});
 
-//예매자 한줄평 헤더부분 업데이트
-function updateReviewHeader(displayInfoSet){
-	let averageStar = document.querySelector(".grade_area .graph_mask .graph_value");
-	let averageScore = document.querySelector(".section_review_list .short_review_area .grade_area .text_value span"); 
-	let totalReviewCount = document.querySelector(".grade_area .join_count em");
-	
-	averageStar.style.width = 100*displayInfoSet.averageScore.toFixed(1)/5.0+"%"; 
-	averageScore.innerHTML = displayInfoSet.averageScore.toFixed(1);
-	totalReviewCount.innerHTML = displayInfoSet.comments.length +"건";
-}
+			function plusSlides(n) {
+				showSlides(sliderIndex += n);
+			}
 
-//예매자 한줄평 업데이트
-function updateReview(displayInfoSet){
-	let reviewBox = document.querySelector("ul.list_short_review");
-	let reviewTemplate = document.querySelector("#reviewTemplate").innerHTML;
-	let bindTemplate = Handlebars.compile(reviewTemplate);
-	
-	let data = {
-			displayInfo : displayInfoSet.displayInfo,
-			comments :  displayInfoSet.comments.slice(0,3)
-	};
-	
-	Handlebars.registerHelper('filePath', function(item) {
-	    return item.commentImages.shift().saveFileName;
-	});
-	
-	Handlebars.registerHelper('countImg', function(item) {
-	    return item.commentImages.length;
-	});
-	
-	reviewBox.innerHTML  = bindTemplate(data);
-	
-}
+			function showSlides(n) {
+				if (sliderIndex == -1) {
+					sliderIndex = totalCount - 1;
+				} else if (sliderIndex === totalCount) {
+					sliderIndex = 0;
+				}
 
-function updateMoreReviewBtn(displayInfoSet){
-	let moreReviewBtn = document.querySelector("a.btn_review_more");
-	moreReviewBtn.setAttribute('href', "/reservationweb/review?id=" + displayInfoSet.displayInfo.displayInfoId);
-}
+				slider.style.transform = "translate(-" + (sliderWidth * sliderIndex) + "px,0)";
+			}
 
-// 상품설명 저장
-function updateProductContent(displayInfoSet){
-	$('.store_details .dsc').html(displayInfoSet.displayInfo.productContent);
-}
+			function pagination() {
+				paginition_current.innerHTML = sliderIndex + 1;
+				paginition_total.innerHTML = totalCount;
+			}
 
-// 상품설명 펼치기-닫기
-function changeProductContent(){
-	$('a.bk_more._open').on('click', function(){
-		$('a.bk_more._open').css('display','none');
-		$('a.bk_more._close').css('display','block');
-		$('div.store_details').removeClass('close3');
-	});
-	$('a.bk_more._close').on('click', function(){
-		$('a.bk_more._open').css('display','block');
-		$('a.bk_more._close').css('display','none');
-		$('div.store_details').addClass('close3');
-	});
-}
-
-// 카태고리탭 정보저장
-function updateDetailTab(displayInfoSet){
-	let content = document.querySelector(".detail_area .detail_info .detail_info_group .detail_info_lst .in_dsc");
-	let title = document.querySelector(".box_store_info .store_name");
-	let newAddr = document.querySelector(".store_addr_wrap .store_addr");
-	let oldAddr = document.querySelector(".store_addr_wrap .store_addr .addr_old_detail");
-	let storeAddr = document.querySelector("p.store_addr.addr_detail");
-	let storeTel= document.querySelector("a.store_tel");
-	let map = document.querySelector("img.store_map");
-	
-	content.innerHTML = displayInfoSet.displayInfo.productContent;
-	title.innerHTML = displayInfoSet.displayInfo.categoryName +" - " + displayInfoSet.displayInfo.productDescription
-	newAddr.innerHTML = displayInfoSet.displayInfo.placeStreet;
-	oldAddr.innerHTML = displayInfoSet.displayInfo.placeLot;
-	storeAddr.innerHTML = displayInfoSet.displayInfo.placeName;
-	storeTel.innerHTML = displayInfoSet.displayInfo.telephone;
-	map.src = "resources/" +  displayInfoSet.displayInfoImage.saveFileName;
-}
-
-
-
-// 상세정보-오시는길 탭 변경
-function changeDetailTab() {
-	let infoTab = document.querySelector(".section_info_tab .info_tab_lst");
-
-	infoTab.addEventListener('click', function(evt) {
-		if (evt.target.tagName === "SPAN" || evt.target.tagName === "A") {
-			changeTab(evt);
-			showContent();
+			function removeSlideBtn() {
+				if (totalCount <= 1) {
+					nextBtn.parentNode.removeChild(nextBtn);
+					prevBtn.parentNode.removeChild(prevBtn);
+				}
+			}
 		}
-	});
+} 
 
-	// 카테고리탭변경
-	function changeTab(evt) {
-		let detailTab = document
+var eventUnit = {
+		// 이벤트정보 업데이트
+		updateEventInfo : 
+			function(displayInfoSet){
+			let discountEvent = document.querySelector(".section_event .event_info_box .event_info .in_dsc");
+			let insertHTML = "[네이버예약 특별할인] <br>";
+			let temp=[];
+
+			displayInfoSet.productPrices.forEach(value=>{
+				temp.push( value.priceTypeName+"석 "+value.discountRate+"%");
+			});
+
+			insertHTML += temp.join(', ') + " 할인";
+			discountEvent.innerHTML = insertHTML;
+		}	
+}
+
+
+var reviewUnit = {
+		// 예매자 한줄평 헤더부분 업데이트
+		updateReviewHeader: 
+			function(displayInfoSet){
+			let averageStar = document.querySelector(".grade_area .graph_mask .graph_value");
+			let averageScore = document.querySelector(".section_review_list .short_review_area .grade_area .text_value span"); 
+			let totalReviewCount = document.querySelector(".grade_area .join_count em");
+
+			averageStar.style.width = 100*displayInfoSet.averageScore.toFixed(1)/5.0+"%"; 
+			averageScore.innerHTML = displayInfoSet.averageScore.toFixed(1);
+			totalReviewCount.innerHTML = displayInfoSet.comments.length +"건";
+		},
+
+		// 예매자 한줄평 업데이트
+		updateReview:
+			function(displayInfoSet){
+			let reviewBox = document.querySelector("ul.list_short_review");
+			let reviewTemplate = document.querySelector("#reviewTemplate").innerHTML;
+			let bindTemplate = Handlebars.compile(reviewTemplate);
+
+			let data = {
+					displayInfo : displayInfoSet.displayInfo,
+					comments :  displayInfoSet.comments.slice(0,3)
+			};
+
+			Handlebars.registerHelper('filePath', function(item) {
+				return item.commentImages.shift().saveFileName;
+			});
+
+			Handlebars.registerHelper('countImg', function(item) {
+				return item.commentImages.length;
+			});
+
+			reviewBox.innerHTML  = bindTemplate(data);
+
+		},
+
+		updateMoreReviewBtn:
+			function(displayInfoSet){
+			let moreReviewBtn = document.querySelector("a.btn_review_more");
+			moreReviewBtn.setAttribute('href', "/reservationweb/review?id=" + displayInfoSet.displayInfo.displayInfoId);
+		}
+
+}
+
+var productContentUnit = {
+		// 상품설명 저장
+		updateProductContent:
+			function(displayInfoSet){
+			$('.store_details .dsc').html(displayInfoSet.displayInfo.productContent);
+		},
+
+		// 상품설명 펼치기-닫기
+		changeProductContent:
+			function(){
+			$('a.bk_more._open').on('click', function(){
+				$('a.bk_more._open').css('display','none');
+				$('a.bk_more._close').css('display','block');
+				$('div.store_details').removeClass('close3');
+			});
+			$('a.bk_more._close').on('click', function(){
+				$('a.bk_more._open').css('display','block');
+				$('a.bk_more._close').css('display','none');
+				$('div.store_details').addClass('close3');
+			});
+		}
+
+}
+
+var detailTabUnit = {
+
+		// 카태고리탭 정보저장
+		updateDetailTab:
+			function(displayInfoSet){
+			let content = document.querySelector(".detail_area .detail_info .detail_info_group .detail_info_lst .in_dsc");
+			let title = document.querySelector(".box_store_info .store_name");
+			let newAddr = document.querySelector(".store_addr_wrap .store_addr");
+			let oldAddr = document.querySelector(".store_addr_wrap .store_addr .addr_old_detail");
+			let storeAddr = document.querySelector("p.store_addr.addr_detail");
+			let storeTel= document.querySelector("a.store_tel");
+			let map = document.querySelector("img.store_map");
+
+			content.innerHTML = displayInfoSet.displayInfo.productContent;
+			title.innerHTML = displayInfoSet.displayInfo.categoryName +" - " + displayInfoSet.displayInfo.productDescription
+			newAddr.innerHTML = displayInfoSet.displayInfo.placeStreet;
+			oldAddr.innerHTML = displayInfoSet.displayInfo.placeLot;
+			storeAddr.innerHTML = displayInfoSet.displayInfo.placeName;
+			storeTel.innerHTML = displayInfoSet.displayInfo.telephone;
+			map.src = "resources/" +  displayInfoSet.displayInfoImage.saveFileName;
+		},
+
+
+
+		// 상세정보-오시는길 탭 변경
+		changeDetailTab:
+			function() {
+			let infoTab = document.querySelector(".section_info_tab .info_tab_lst");
+
+			infoTab.addEventListener('click', function(evt) {
+				if (evt.target.tagName === "SPAN" || evt.target.tagName === "A") {
+					changeTab(evt);
+					showContent();
+				}
+			})},
+
+			// 카테고리탭변경
+			changeTab:
+				function(evt) {
+				let detailTab = document
 				.querySelector(".info_tab_lst ._detail .anchor span");
-		let locationTab = document
+				let locationTab = document
 				.querySelector(".info_tab_lst ._path .anchor span");
-		let currentTab = document.querySelector("a.anchor.active span");
+				let currentTab = document.querySelector("a.anchor.active span");
 
-		if (evt.target === detailTab || evt.target === locationTab) {
-			if (!(evt.target === currentTab)) {
-				currentTab.parentNode.classList.remove('active');
-				evt.target.parentNode.classList.add('active');
+				if (evt.target === detailTab || evt.target === locationTab) {
+					if (!(evt.target === currentTab)) {
+						currentTab.parentNode.classList.remove('active');
+						evt.target.parentNode.classList.add('active');
+					}
+				} else if (evt.target === detailTab.parentNode
+						|| evt.target === locationTab.parentNode) {
+					if (!(evt.target === currentTab.parentNode)) {
+						currentTab.parentNode.classList.remove('active');
+						evt.target.classList.add('active');
+					}
+				}
+			},
+
+			// 카테고리탭 내용 전환
+			showContent :
+				function(){
+				let detailContent = document.querySelector("div.detail_area_wrap");
+				let locationContent = document.querySelector("div.detail_location");
+				let currentTab = document.querySelector("a.anchor.active span").innerHTML;
+
+				if(currentTab === "상세정보"){
+					detailContent.classList.remove("hide");
+					locationContent.classList.add("hide");
+				}else if(currentTab === "오시는길"){
+					detailContent.classList.add("hide");
+					locationContent.classList.remove("hide");
+				}
 			}
-		} else if (evt.target === detailTab.parentNode
-				|| evt.target === locationTab.parentNode) {
-			if (!(evt.target === currentTab.parentNode)) {
-				currentTab.parentNode.classList.remove('active');
-				evt.target.classList.add('active');
-			}
-		}
-	}
-	
-	// 카테고리탭 내용 전환
-	function showContent(){
-		let detailContent = document.querySelector("div.detail_area_wrap");
-		let locationContent = document.querySelector("div.detail_location");
-		let currentTab = document.querySelector("a.anchor.active span").innerHTML;
-		
-		if(currentTab === "상세정보"){
-			detailContent.classList.remove("hide");
-			locationContent.classList.add("hide");
-		}else if(currentTab === "오시는길"){
-			detailContent.classList.add("hide");
-			locationContent.classList.remove("hide");
-		}
-	}
-	
+
 }
-
 
 function pressRsvBtn(){
 	location.href = "/reservationweb/reserve?id=" + getParam("id");
